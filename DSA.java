70d0d14e15082cbd6d8092d5a9aa0c54d53319e3
mattk <mkimucsb@gmail.com>
2024-08-21 13:13:06 -0700

    one more leetcode

--- DSA.java
+++ DSA.java
@@ -1,16 +1,18 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class DSA {
 
-    public boolean containsDuplicate(int[] nums) {
+    public boolean containsDuplicate(int[] numbs) {
         Set<Integer> noDupes = new HashSet<>();
-        for (int num : nums) {
+        for (int num : numbs) {
             noDupes.add(num);
         }
 
-        if (noDupes.size() < nums.length) {
+        if (noDupes.size() < numbs.length) {
             return true;
         } else {
             return false;
@@ -24,14 +26,14 @@ public class DSA {
     // Time complexity: O(n)
     // Space complexity: O(n)
 
-        public boolean isAnagram(String s, String t) {
+    public boolean isAnagram(String s, String t) {
 
         char[] sArray = s.toCharArray();
         char[] tArray = t.toCharArray();
 
         Arrays.sort(sArray);
         Arrays.sort(tArray);
-        
+
         return Arrays.equals(sArray, tArray);
     }
 
@@ -41,8 +43,30 @@ public class DSA {
     // return the result
     // Time complexity: O(n log n)
     // Space complexity: O(n)
-    
 
+    public int[] twoSum(int[] numb, int target) {
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        int[] solutions = new int[2];
+        for (int i = 0; i < numb.length; i++) {
+            int remainder = target - numb[i];
+            if (hashMap.get(remainder) != null) {
+                solutions[0] = hashMap.get(remainder);
+                solutions[1] = i;
+                break;
+            } else {
+                hashMap.put(numb[i], i);
+            }
+        }
+        return solutions;
+    }
+
+    // create a hashmap to store the numbers
+    // iterate through the array
+    // calculate the remainder
+    // if the remainder is in the hashmap, return the indices
+    // else add the number to the hashmap
+    // Time complexity: O(n)
+    // Space complexity: O(n)
 
 
 }
