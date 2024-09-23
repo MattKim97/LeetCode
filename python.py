af6b0b1f58e3196c12ae9e3b95e39494e5f4c84a
mattk <mkimucsb@gmail.com>
2024-09-23 15:57:12 -0700

    intro

--- python.py
+++ python.py
@@ -1,4 +1,10 @@
 # programming act of telling a computer what to do
-#  terminal is also known as the command-line, shell command prompt or console
-#  the Python programming language was released in 1991 by Guido van Rossum
-#  Python is an open-source programming language that is free to use and distribute
+# terminal is also known as the command-line, shell command prompt or console
+# the Python programming language was released in 1991 by Guido van Rossum
+# Python is an open-source programming language that is free to use and distribute
+# Python has a small formal and rigid syntax
+# includes identifiers, keywords, statements, code blocks, whitespace, and comments
+# a variable is an identifier that labels a value
+# a variable's value can change
+# name is important for a variable so we remember what it is stored as
+# each variable has a data type, inferred by the value assigned to it
