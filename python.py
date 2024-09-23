63671d308e652ada8040f001bfdbe4e040fc76a7
mattk <mkimucsb@gmail.com>
2024-09-23 15:54:59 -0700

    python refresehers

---
+++ python.py
@@ -0,0 +1,4 @@
+# programming act of telling a computer what to do
+#  terminal is also known as the command-line, shell command prompt or console
+#  the Python programming language was released in 1991 by Guido van Rossum
+#  Python is an open-source programming language that is free to use and distribute
