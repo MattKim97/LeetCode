260b50f335c07f444d56b31f692096fce2b93848
mattk <mkimucsb@gmail.com>
2024-10-30 16:34:06 -0700

    explanation

--- DSA.java
+++ DSA.java
@@ -545,24 +545,33 @@ public class DSA {
     // Time complexity: O(n)
     // Space complexity: O(n)
 
-    class Solution {
-        public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-         if (root == null) return false;
-            if (isSameTree(root, subRoot)) return true;
+    // class Solution {
+    //     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+    //      if (root == null) return false;
+    //         if (isSameTree(root, subRoot)) return true;
             
-            return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
-        }
+    //         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    //     }
         
-        private boolean isSameTree(TreeNode s, TreeNode t) {
-            if (s == null && t == null) return true;
-            if (s == null || t == null) return false; 
-            if (s.val != t.val) return false; /
+    //     private boolean isSameTree(TreeNode s, TreeNode t) {
+    //         if (s == null && t == null) return true;
+    //         if (s == null || t == null) return false; 
+    //         if (s.val != t.val) return false; /
             
-            return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
-        }
-    }
+    //         return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    //     }
+    // }
+
+    // check if the root is null
+    // check if the trees are the same
+    // recursively call the function on the left and right nodes
+    // return the result
+    // Time complexity: O(n)
+    // Space complexity: O(n)
 
     
+
+
     
 
 }
