bc45332cf4dea46b0b5717cf5598a44aa1a47945
mattk <mkimucsb@gmail.com>
2024-11-11 22:28:00 -0800

    bubble sort

--- javascript.js
+++ javascript.js
@@ -94,3 +94,20 @@ const dfs = (graph, start) => {
 }
 
 
+const bubblesort = (arr) => {
+    let swapped = false
+    do {
+        swapped = false
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] > arr[i+1]){
+                let temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                swapped = true
+            }
+        }
+    } while(swapped)
+    return arr
+}
+
+
