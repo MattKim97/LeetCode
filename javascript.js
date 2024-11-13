cd4a47cb096cf47040547b580f936cd4225d8f3c
mattk <mkimucsb@gmail.com>
2024-11-12 18:30:55 -0800

    insertionsort

--- javascript.js
+++ javascript.js
@@ -126,3 +126,16 @@ const quicksort = (arr) => {
     }
     return [...quicksort(left),pivot,...quicksort(right)]
 }
+
+const insertionsort = (arr) => {
+    for(let i = 1; i < arr.length; i++){
+        let j = i
+        while(j > 0 && arr[j-1] > arr[j]){
+            let temp = arr[j]
+            arr[j] = arr[j-1]
+            arr[j-1] = temp
+            j--
+        }
+    }
+    return arr
+}
