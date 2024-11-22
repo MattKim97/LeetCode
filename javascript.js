2b76b1c9fd16cf9c47c18611f11292046b197eba
mattk <mkimucsb@gmail.com>
2024-11-22 10:54:17 -0800

    selection sort

--- javascript.js
+++ javascript.js
@@ -139,3 +139,21 @@ const insertionsort = (arr) => {
     }
     return arr
 }
+
+const selectionsort = (arr) => {
+    for(let i = 0; i < arr.length; i++){
+        let min = i
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[j] < arr[min]){
+                min = j
+            }
+        }
+        if(i !== min){
+            let temp = arr[i]
+            arr[i] = arr[min]
+            arr[min] = temp
+        }
+    }
+    return arr
+}
+
