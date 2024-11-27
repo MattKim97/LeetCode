80e6228e26e026e57c7013016d1b7b96a3c70959
mattk <mkimucsb@gmail.com>
2024-11-26 17:04:52 -0800

    bs

--- javascript.js
+++ javascript.js
@@ -172,3 +172,22 @@ console.log(bubblesort([5,4,3,2,1]))
 console.log(quicksort([5,4,3,2,1]))
 console.log(insertionsort([5,4,3,2,1]))
 console.log(selectionsort([5,4,3,2,1]))
+
+
+const binarySearch = (arr, target) => {
+    let left = 0
+    let right = arr.length - 1
+    while(left <= right){
+        let mid = Math.floor((left + right) / 2)
+        if(arr[mid] === target){
+            return mid
+        }
+        if(arr[mid] < target){
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return -1
+}
+
