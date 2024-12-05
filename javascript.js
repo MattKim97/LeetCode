60c8df7e519d68aef4f816f08f8c17d2e0f503f6
mattk <mkimucsb@gmail.com>
2024-12-04 19:11:17 -0800

    recurseive binary

--- javascript.js
+++ javascript.js
@@ -196,3 +196,19 @@ console.log(binarySearch([1,2,3,4,5],3))
 console.log(binarySearch([1,2,3,4,5],6))
 console.log(binarySearch([1,2,3,4,5],0))
 console.log(binarySearch([1,2,3,4,5],1))
+
+
+const binarySearchRecursive = (arr, target, left = 0, right = arr.length - 1) => {
+    if(left > right){
+        return -1
+    }
+    let mid = Math.floor((left + right) / 2)
+    if(arr[mid] === target){
+        return mid
+    }
+    if(arr[mid] < target){
+        return binarySearchRecursive(arr, target, mid + 1, right)
+    } else {
+        return binarySearchRecursive(arr, target, left, mid - 1)
+    }
+}
